d388451350ad182604a341a59f4865d88393b445
Combatman96 <84495690+Combatman96@users.noreply.github.com>
2022-01-02 20:33:26 +0700

    Open locker and key pickup
    Mở tủ và lấy chìa khóa

---
+++ Assets/Scripts/OpenDrawer.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class OpenDrawer : MonoBehaviour
+{
+    public Transform drawer;
+    public float openingSpeed = 3f;
+
+    private bool isOpen = false;
+    private int num = 0;
+    private Vector3 openPos;
+    public void Open(){
+        if(num == 0){
+            isOpen = true;
+            openPos = drawer.position + new Vector3(1.2f, 0f, 0f);
+            num ++;
+        }
+    }
+
+    private void Update() {
+        if(isOpen){
+            drawer.position = Vector3.MoveTowards(drawer.position, openPos, openingSpeed*Time.deltaTime);
+        }
+    }
+}
