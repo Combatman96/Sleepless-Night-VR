7acf6c1e5571a5046a674d40618313532f660854
Combatman96 <84495690+Combatman96@users.noreply.github.com>
2022-01-04 17:08:52 +0700

    fix the dam door

--- Assets/Huy/Scripts/Door.cs
+++ Assets/Huy/Scripts/Door.cs
@@ -3,29 +3,29 @@ using TMPro;
 public class Door : MonoBehaviour
 {
     [Header("Related GameObjects")]
-    public GameObject parent;
-    public GameObject model;
     public TextMeshProUGUI inventoryLabel;
-    public Collider boxCollider;
+    private Collider boxCollider;
     private Animator animator;
 
     private AudioSource doorSfx;
 
     [Header("Properties")]
+    public bool unlocked = false;
     public int /* Item */ keyId;
     public Inventory inventory;
     private bool is_unlocked = false;
 
     void Start()
     {
-        animator = parent.GetComponent<Animator>();
+        boxCollider = GetComponent<BoxCollider>();
+        animator = GetComponent<Animator>();
         doorSfx = GetComponent<AudioSource>();
     }
 
     public void OpenTheDoor()
     {
-        if(inventory.getKey(keyId)){
-            animator.SetBool("is_unlocked", true);
+        if(inventory.getKey(keyId) || unlocked){
+            animator.SetBool("isOpen", true);
             doorSfx.Play();
             inventory.DiscardKey(keyId);
             boxCollider.enabled = false;
