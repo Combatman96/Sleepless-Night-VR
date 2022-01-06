b6f8fa6166f69bed435d824ab818a6ea059b020b
use25 <55612389+use25@users.noreply.github.com>
2022-01-07 04:17:10 +0700

    Optimizing game

---
+++ Assets/Huy/Scripts/ColliderAppearance.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class ColliderAppearance : MonoBehaviour
+{
+    public GameObject[] target;
+    public bool makesDisappear;
+
+    void OnTriggerEnter(Collider col)
+    {
+        if(col.gameObject.tag == "Player")
+        {
+            foreach (GameObject i in target)
+            {
+                if (makesDisappear)
+                    i.SetActive(false);
+                else i.SetActive(true);
+            }
+        }
+    }
+}
